83bc6d7c851e297d1f2414a741c93f7e68717a41
Morgen Diaz <morgen@webslinger.works>
2020-11-05 19:44:23 -1000

    Created single Activity project with template Fragment and Viewmodels. Setup basic directory structure.

---
+++ app/src/main/java/com/webslinger/dejavu/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.webslinger.dejavu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.webslinger.dejavu.presentation.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
